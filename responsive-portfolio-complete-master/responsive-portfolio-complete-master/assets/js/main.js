ac96e3c1f4592fca85b823072d769c53b616d891
yayayaloo <168859143+yayayaloo@users.noreply.github.com>
2024-05-05 19:54:14 +0800

    Add files via upload

---
+++ responsive-portfolio-complete-master/responsive-portfolio-complete-master/assets/js/main.js
@@ -0,0 +1,17 @@
+/*===== MENU SHOW Y HIDDEN =====*/
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
+    closeMenu = document.getElementById('nav-close')
+
+// SHOW
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
+
+// HIDDEN
+
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+
+
+/*===== SCROLL SECTIONS ACTIVE LINK =====*/
